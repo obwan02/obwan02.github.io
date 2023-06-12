5b9dd1d330900b13bf787701c0633014ecc5cea5
obwan02 <obwan02@hotmail.co.nz>
2023-06-13 11:36:04 +1200

    update: Improved construction message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to obwan02.github.io</h1>
-<p>Current under construction - come back soon!</p>
+<div>
+    <h1>Welcome to obwan02.github.io</h1>
+    <p>Currently under construction - come back soon!</p>
+</div>
+
+<style>
+
+ div {
+    width: 100%;
+    margin-top: 40vh;
+ }
+
+ div > * {
+     margin-top: 1rem;   
+     text-align: center;
+ }
+
+ * {
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+ }   
+</style>
